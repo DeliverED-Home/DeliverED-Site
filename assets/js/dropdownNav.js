10bfccb91f7066f3dd83c98f4d5ba73577d3f4ba
C4R15 <chrispercevalmaxwell@gmail.com>
2021-03-09 22:13:44 +0000

    playing w navbar 6

--- assets/js/dropdownNav.js
+++ assets/js/dropdownNav.js
@@ -3,4 +3,7 @@ $(window).ready(function(){
   $('a#dropdown-btn').on('click', function() {
 		$('div#dropdown').slideToggle(700);
 	});
+  $('div#dropdown>div>div>a').on('click', function() {
+		$('div#dropdown').slideToggle(400);
+	});
 })
