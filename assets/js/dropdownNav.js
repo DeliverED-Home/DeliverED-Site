fc7b0ac0653d817566e83aa9cf6f9d0bff139f86
C4R15 <chrispercevalmaxwell@gmail.com>
2021-03-10 00:57:10 +0000

    ok i think things are fine. and tweaks to nav bar again

--- assets/js/dropdownNav.js
+++ assets/js/dropdownNav.js
@@ -2,6 +2,8 @@ $(window).ready(function(){
   $('div#dropdown').hide()
   $('a#dropdown-btn').on('click', function() {
 		$('div#dropdown').slideToggle(700);
+    // $('a#dropdown-btn>i').removeClass('fa-chevron-down');
+    $('a#dropdown-btn>i').toggleClass('fa-chevron-up');
 	});
   $('div#dropdown>div>div>a').on('click', function() {
 		$('div#dropdown').slideToggle(400);
