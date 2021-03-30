005618b7332dd6951e5fc609417b90cafa327852
C4R15 <chrispercevalmaxwell@gmail.com>
2021-03-30 18:56:07 +0100

    added reports and vids

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -19,19 +19,4 @@ window.onload = function() {
     img.style.top = 7 + (-0.5 * Math.sqrt(scrollPercent * window.scrollY)) * 0.5 + 'vw';
     img.style.right = 2.2 + (-0.5 * Math.sqrt(scrollPercent * window.scrollY)) * 0.5 + 'vw';
   });
-
-  var counter = 0;
-  var counter2 = 0;
-  document.getElementById('sys-nav').onclick = function () {
-    if (counter == 0) {
-      document.getElementById('ad-vid').play();
-      counter++;
-    }
-  };
-  document.getElementById('sys-nav-2').onclick = function () {
-    if (counter2 == 0) {
-      document.getElementById('ad-vid-s').play();
-      counter++;
-    }
-  };
 }
